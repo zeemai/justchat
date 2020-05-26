5ba0b32d8cb3ed565be1a2571b7c9e1ad9c83c8b
Zee Mai <zihao_mai@hotmail.com>
2020-05-26 23:33:04 +0100

    Added @material-ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Dashboard from './Dashboard';
+
 function App() {
   return (
     <div className="App">
-
+      <Dashboard />
     </div>
   );
 }
